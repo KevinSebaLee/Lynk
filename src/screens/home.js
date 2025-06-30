a5fc2d50a4117385785e2cda74c0ce55a34c06a0
iachuDyner <48313765@est-ort.edu.ar>
2025-06-30 08:50:30 -0300

    Actualizacion de la home

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, SafeAreaView, Text, Image , Dimensions, Pressable, Button} from 'react-native';
-import Header from '../components/header.js';
-import Container from '../components/container.js';
-import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  Image,
+  Dimensions,
+  Pressable,
+  Button,
+} from "react-native";
+import Header from "../components/header.js";
+import Container from "../components/container.js";
+import { LinearGradient } from "expo-linear-gradient";
 //import Carousel from 'react-native-snap-carousel';
-import tickets from './tickets.js';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import premiumGeneral from './premiumGeneral.js';
-import eventos from './eventos.js';
-import TicketCard from '../components/TicketCard.js';
-import PremiumBanner from '../components/premiumBanner';
+import tickets from "./tickets.js";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import premiumGeneral from "./premiumGeneral.js";
+import eventos from "./eventos.js";
+import TicketCard from "../components/TicketCard.js";
+import PremiumBanner from "../components/premiumBanner";
 
-
-const width = Dimensions.get('window').width;
+const width = Dimensions.get("window").width;
 
 export default function home() {
-  
   // const carouselItems = [
   //   { title: 'Promo 1', text: '50% en tu primera compra' , bgImage: require('./assets/img/lollapalooza.jpg')},
   //   { title: 'Promo 2', text: '2x1 en recargas los lunes' },
@@ -23,49 +30,34 @@ export default function home() {
   // ];
   const navigation = useNavigation();
 
-
   return (
-
-
-   
-
-<>
-    <View style={styles.container}>
-      <LinearGradient colors={['#642684', '#ffffff']} style={{ flex: 1 }}>
+    <LinearGradient colors={["#642684", "#ffffff"]} style={StyleSheet.absoluteFillObject}>
+      <View style={styles.container}>
         <StatusBar style="light" />
-        <Header nombre='Kevin' />
-
-   
+        <Header nombre="Kevin" />
 
         <Pressable onPress={() => navigation.navigate(tickets)}>
           <SafeAreaView style={styles.container}>
-            <TicketCard onGetMore={() => alert('¡Función para conseguir más tickets!')} />
+            <TicketCard
+              onGetMore={() =>
+                alert("¡Función para conseguir más tickets!")
+              }
+            />
           </SafeAreaView>
         </Pressable>
-
-        
-
-
-        {
-        /*< Pressable onPress={() => navigation.navigate(eventos)} style={{backgroundColor:'gray', marginTop:20}}>
-          <Text>Nombre Evento</Text>
-          <Button title="Ver más"/>
-        </Pressable> */}
-      </LinearGradient>
-    </View>
-    <View>
-      <PremiumBanner/>
-    </View>
-    </>
+      </View>
+      <View>
+        <PremiumBanner />
+      </View>
+    </LinearGradient>
   );
 }
-
 
 const styles = StyleSheet.create({
   carouselItem: {
     flex: 1,
     borderRadius: 12,
-    overflow: 'hidden',
+    overflow: "hidden",
     marginHorizontal: 10,
   },
   carousel: {
@@ -73,39 +65,39 @@ const styles = StyleSheet.create({
   },
   carouselBackground: {
     ...StyleSheet.absoluteFillObject,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   container: {
     flex: 0.6,
-  },  
+  },
   tickets: {
-    backgroundColor: '#ffffff',
+    backgroundColor: "#ffffff",
     borderRadius: 10,
     paddingVertical: 25,
     paddingHorizontal: 20,
   },
   textoTicket: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingRight: 10,
     paddingLeft: 5,
     marginBottom: 10,
   },
   ticketTotal: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     marginBottom: 10,
   },
   cantidadTickets: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginTop: 10,
   },
   numeroTickets: {
     fontSize: 40,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   iconTicket: {
     width: 40,
@@ -121,10 +113,10 @@ const styles = StyleSheet.create({
     marginTop: 15,
   },
   metodoPago: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: '#000000',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#000000",
     borderRadius: 5,
     paddingHorizontal: 18,
     paddingVertical: 12,
@@ -138,17 +130,17 @@ const styles = StyleSheet.create({
     height: 24,
   },
   movimientosText: {
-    color: '#9F4B97',
-    fontWeight: '500',
+    color: "#9F4B97",
+    fontWeight: "500",
   },
   planPremium: {
     borderRadius: 10,
     marginTop: 20,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
-  tituloPremium:{
+  tituloPremium: {
     fontSize: 23,
-    color: '#ffffff',
-    textDecorationStyle: 'solid'
-  },  
+    color: "#ffffff",
+    textDecorationStyle: "solid",
+  },
 });
